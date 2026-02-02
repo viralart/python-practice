478976798d50daa3b17cf53159f76eb785b1972c
Viral Not QA <viral.maurya@artoon.in>
2026-02-02 13:32:20 +0530

    Commit

--- pricefilter.py
+++ pricefilter.py
@@ -12,13 +12,16 @@
 
 
 
-def removed_dollar(price):
-    amount = float(price.replace("$",""))
-    for el in price:
-        if amount > 20: return amount
-        else : return None
+def clear_price(price):
+    return float(price.replace("$",""))
 
 
 inventory_prices = ["$29.99","$9.99","$15.99","$49.99","$7.99","$10.00"]
-result = removed_dollar(inventory_prices)
-print(result)
+expensive_items = []
+for p in inventory_prices:
+    amount = clear_price(p)
+
+    if amount > 20: 
+        expensive_items.append(amount)
+
+print(expensive_items)
